926b83c2ba56d3b76a566daee1e6824bd0ee406e
Mauricio Duarte <prussdev@gmail.com>
2019-10-07 10:12:38 -0300

    Endpoint para buscar Cliente por email

--- src/main/java/com/mauriciopd/cursomc/resources/ClienteResource.java
+++ src/main/java/com/mauriciopd/cursomc/resources/ClienteResource.java
@@ -40,6 +40,12 @@ public class ClienteResource {
 		return ResponseEntity.ok(obj);
 	}
 	
+	@GetMapping(value = "/email")
+	public ResponseEntity<Cliente> find(@RequestParam(value = "value") String email) {
+		Cliente obj = service.findByEmail(email);
+		return ResponseEntity.ok(obj);
+	}
+	
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
 		Cliente obj = service.fromDTO(objDto);
